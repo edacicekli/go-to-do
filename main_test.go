048d1e2bd6d4b43dde0aad94d1510a450ede8cc0
Eda Çiçekli <edaciceklii@gmail.com>
2021-10-30 16:06:22 +0300

    update test

--- main_test.go
+++ main_test.go
@@ -1,9 +1,22 @@
 package main
-import "testing"
 
-func TestRemoveTodo(t *testing.T){ 
-	removeTodo("0")
-	if len(todos)!=3{
+import (
+	"testing"
+	"time"
+)
+
+func TestRemoveTodo(t *testing.T) {
+
+	newTodo := Todo{ID: "0", IsFinished: false, Title: "mocktodotitle", Body: "mocktodobody", DueDate: time.Now()}
+	todos = append(todos, newTodo)
+
+	if len(todos) != 1 {
 		t.Fatalf("TestRemoveTodo not passed")
 	}
+
+	removeTodo("0")
+	if len(todos) != 0 {
+		t.Fatalf("TestRemoveTodo not passed")
+	}
+
 }
